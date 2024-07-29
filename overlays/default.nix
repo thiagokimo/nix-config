4a30ec877d4700e0098c0f58212192c8ea07ea6c
Thiago Rocha <kimo@kimo.io>
2024-07-29 15:04:47 -0300

    Add boilerplates for pkgs and overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,3 +1,15 @@
-{ ... }: {
-
+{ inputs, ... }: {
+  additions = final: _prev: import ../pkgs final.pkgs;
+  modifications = final: prev: {
+    # TODO Add an overlay for Steam
+    # steamOverlay = prev.steam.overrideAttrs (oldAttrs: rec {
+    # ...
+    # });
+  };
+  unstable-packages = final: _prev: {
+    unstable = import inputs.nixpkgs-unstable {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  };
 }
