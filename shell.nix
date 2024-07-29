73a7aa5c4a5d735f51ed933b3cb0d78648f91376
Thiago Rocha <kimo@kimo.io>
2024-07-29 15:00:53 -0300

    Addds a shellhook and a name for the main dev shell

--- shell.nix
+++ shell.nix
@@ -1,10 +1,15 @@
-{pkgs ? import <nixpkgs> {}, ...}: {
+{ pkgs ? import <nixpkgs> {}, ...}: {
   default = pkgs.mkShell {
-    NIX_CONFIG = "extra-experimental-features = nix-command flakes"; 
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs; [
       nix
       home-manager
       git
     ];
+    name = "my-nix-config-shell";
+    shellHook = ''
+      echo "Welcome to the playground!"
+    '';
   };
 }
+
