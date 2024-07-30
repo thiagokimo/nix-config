42a8f424546ded5ca07c6fb801cc0d3cdfaa04f2
Thiago Rocha <kimo@kimo.io>
2024-07-30 16:24:03 -0300

    Only use nixpkgs unstable (for now)

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "Home Sweet Home";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
--- overlays/default.nix
+++ overlays/default.nix
@@ -7,7 +7,7 @@
     # });
   };
   unstable-packages = final: _prev: {
-    unstable = import inputs.nixpkgs-unstable {
+    unstable = import inputs.nixpkgs {
       system = final.system;
       config.allowUnfree = true;
     };
