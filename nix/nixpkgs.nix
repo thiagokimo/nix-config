13830e55d2e30b489cced572219e845eb3f1b766
Thiago Rocha <kimo@kimo.io>
2024-08-01 22:21:20 -0300

    Disable overlays for now

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -9,11 +9,11 @@
   };
 
   nixpkgs = {
-    overlays = [
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
-    ];
+    # overlays = [
+    #   outputs.overlays.additions
+    #   outputs.overlays.modifications
+    #   outputs.overlays.unstable-packages
+    # ];
     config = {
       allowUnfree = true;
       allowUnfreePredicate = _: true;
