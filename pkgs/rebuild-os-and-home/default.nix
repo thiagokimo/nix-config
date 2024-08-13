3f94692ef1cc06734f20ba30a8464f073bd4c96e
Thiago Rocha <kimo@kimo.io>
2024-08-13 15:19:31 -0300

    Adding package to bring up a given OS and home specs

--- pkgs/rebuild-os-and-home/default.nix
+++ pkgs/rebuild-os-and-home/default.nix
@@ -12,7 +12,11 @@
     echo "Exiting"
     exit 0
   else
+    echo "Rebuilding OS for ${hostName}"
     sudo nixos-rebuild switch --flake github:thiagokimo/nix-config#${hostName}
+    echo "Rebuilding home of ${homeName}"
     home-manager switch --flake github:thiagokimo/nix-config#${homeName}
+
+    echo "Done"
   fi
 ''
