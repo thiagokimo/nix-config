3716c1db3c6fbd73d235ba87c02186417484dd47
Thiago Rocha <kimo@kimo.io>
2024-08-13 12:47:58 -0300

    Fixes typo

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,4 @@
 {pkgs ? import <nixpkgs>, ...}: rec {
   create-home-folders = pkgs.callPackage ./create-home-folders {};
-  rebuild-os-and-home = pkgs.callPackcage ./rebuild-os-and-home;
+  rebuild-os-and-home = pkgs.callPackage ./rebuild-os-and-home;
 }
