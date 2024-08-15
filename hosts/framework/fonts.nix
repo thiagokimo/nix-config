db41bd31933572382b305cbf322e1ac4859d31da
Thiago Rocha <kimo@kimo.io>
2024-08-15 20:26:18 -0300

    Remove duplicated framework files and old cli home

--- hosts/framework/fonts.nix
+++
@@ -1,7 +0,0 @@
-{pkgs, ...}: {
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-emoji
-    noto-fonts-cjk
-  ];
-}
--- hosts/framework/system.nix
+++
@@ -1,8 +0,0 @@
-{pkgs, ...}: {
-  time.timeZone = "America/Sao_Paulo";
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-  nixpkgs.config.allowUnfree = true;
-
-  system.stateVersion = "24.05";
-}
