c2cdbdbb75227b276086d39ed48d5652397b96f6
Thiago Rocha <kimo@kimo.io>
2024-08-19 09:37:40 -0300

    Adding a nix config file into thiago's home

---
+++ homes/thiago/nixpkgs.nix
@@ -0,0 +1,14 @@
+{...}: {
+  nix = {
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
+}
