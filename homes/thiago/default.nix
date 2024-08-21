aacfa97950d1a2f641feb03d5a6be0e13c2fc12b
Thiago Rocha <kimo@kimo.io>
2024-08-21 20:53:57 -0300

    Fixes nix missing attribute and add custom scripts into thiago's home

--- homes/thiago/default.nix
+++ homes/thiago/default.nix
@@ -1,19 +1,15 @@
 {username, ...}: {
-
   imports = [
     ../../modules/home-manager/programs/cli
+    ../../modules/home-manager/scripts
     ./nixpkgs.nix
   ];
 
   home = {
     username = "${username}";
     homeDirectory = "/home/${username}";
-    sessionVariables = {
-      EDITOR = "vim";
-    };
-
     stateVersion = "24.05";
   };
-  
+
   programs.home-manager.enable = true;
 }
