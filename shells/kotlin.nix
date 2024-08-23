58ff24d23ec0eb24075d74cb1205bb5701984d14
Thiago Rocha <kimo@kimo.io>
2024-08-23 18:13:38 -0300

    Formatting shells

--- shells/kotlin.nix
+++ shells/kotlin.nix
@@ -1,12 +1,17 @@
-{nixpkgs, system, inputs, ...}: let
+{
+  nixpkgs,
+  system,
+  inputs,
+  ...
+}: let
   pkgs = nixpkgs.legacyPackages.${system};
-in pkgs.mkShell {
-  buildInputs = with pkgs; [
-    jetbrains.idea-community
-  ];
-  name = "Kotlin environment";
-  shellHook = ''
-    echo "Entering Kotlin development environment"
-  '';
-}
-
+in
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+      jetbrains.idea-community
+    ];
+    name = "Kotlin environment";
+    shellHook = ''
+      echo "Entering Kotlin development environment"
+    '';
+  }
--- shells/nix.nix
+++ shells/nix.nix
@@ -1,17 +1,23 @@
-{nixpkgs, system, inputs, ...}: let
+{
+  nixpkgs,
+  system,
+  inputs,
+  ...
+}: let
   pkgs = nixpkgs.legacyPackages.${system};
-in pkgs.mkShell {
-  env = {
-    NIX_CONFIG = "extra-experimental-features = nix-command flakes";
-  };
-  # NIX_CONFIG = "extra-experimental-features = nix-command flakes";
-  nativeBuildInputs = with pkgs; [
-    alejandra
-    home-manager
-    nix
-  ];
-  name = "Nix environment";
-  shellHook = ''
-    echo "Entering Nix development environment"
-  '';
-}
+in
+  pkgs.mkShell {
+    env = {
+      NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+    };
+    # NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+    nativeBuildInputs = with pkgs; [
+      alejandra
+      home-manager
+      nix
+    ];
+    name = "Nix environment";
+    shellHook = ''
+      echo "Entering Nix development environment"
+    '';
+  }
