76938d96a6e891f69d17a3400458c760411168c1
Thiago Rocha <kimo@kimo.io>
2024-08-23 18:13:38 -0300

    Formatting shells

--- shells/blank.nix
+++ shells/blank.nix
@@ -1,8 +1,13 @@
-{nixpkgs, system, inputs, ...}: let
+{
+  nixpkgs,
+  system,
+  inputs,
+  ...
+}: let
   pkgs = nixpkgs.legacyPackages.${system};
-in pkgs.mkShell {
-  buildInputs = with pkgs; [];
-  name = "";
-  shellHook = '''';
-}
-
+in
+  pkgs.mkShell {
+    buildInputs = with pkgs; [];
+    name = "";
+    shellHook = '''';
+  }
--- shells/go.nix
+++ shells/go.nix
@@ -1,17 +1,22 @@
-{nixpkgs, system, inputs, ...}: let
+{
+  nixpkgs,
+  system,
+  inputs,
+  ...
+}: let
   pkgs = nixpkgs.legacyPackages.${system};
-in pkgs.mkShell {
-  buildInputs = with pkgs; [
-    go
-    go-outline
-    gopls
-    gopkgs
-    go-tools
-    delve
-  ];
-  name = "Go environment";
-  shellHook = ''
-    echo "Entering Go development environment"
-  '';
-}
-
+in
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+      go
+      go-outline
+      gopls
+      gopkgs
+      go-tools
+      delve
+    ];
+    name = "Go environment";
+    shellHook = ''
+      echo "Entering Go development environment"
+    '';
+  }
