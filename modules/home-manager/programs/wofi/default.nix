034ffc0d945128c7ff7877732b29f5768f6afc40
Thiago Rocha <kimo@kimo.io>
2024-09-04 17:46:46 -0300

    Adding wofi stuff

---
+++ modules/home-manager/programs/wofi/default.nix
@@ -0,0 +1,13 @@
+{...}: {
+  programs.wofi = {
+    enable = true;
+    settings = {
+      allow_markup = true;
+      show = "drun";
+      prompt = "Apps";
+      key_exit = "Escape";
+      key_expand = "Tab";
+      sort_order = "default";
+    };
+  };
+}
