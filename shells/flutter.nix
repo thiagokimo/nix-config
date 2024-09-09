0ac1ae5f75b83fcda6f8573184e18de0107ec56c
Thiago Rocha <kimo@kimo.io>
2024-09-09 11:22:16 -0300

    Adds a shell to develop flutter

---
+++ shells/flutter.nix
@@ -0,0 +1,14 @@
+{pkgs ? import <nixpkgs> {}, ...}:
+pkgs.mkShell {
+  env = {
+    CHROME_EXECUTABLE = pkgs.chromedriver + "/bin/chromedriver";
+  };
+  nativeBuildInputs = with pkgs; [
+    flutter
+    dart
+    androidSdk
+    jdk11
+  ];
+  name = "Flutter Dev Environment";
+  shellHook = "Welcome to the flutter dev environment shell";
+}
