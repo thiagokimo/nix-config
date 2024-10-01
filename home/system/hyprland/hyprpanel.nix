b5c06b6c7f2e85dd8aa90e5a226a94b59d6578ab
Thiago Rocha <kimo@kimo.io>
2024-10-01 17:44:50 -0300

    Upscale to 1.25. Adding a small config file for hyprpanel

--- home/system/hyprland/hyprpanel.nix
+++ home/system/hyprland/hyprpanel.nix
@@ -8,4 +8,31 @@
   ];
 
   home.packages = with pkgs; [hyprpanel libnotify];
+
+  home.file.".cache/ags/hyprpanel/options.json" = {
+    text = ''
+      {
+        "bar.layouts" = {
+          "*": {
+            "left": [
+              "dashboard",
+              "workspaces",
+            ],
+            "middle": [
+              "windowtitle"
+            ],
+            "right": [
+              "systray",
+              "network",
+              "volume",
+              "battery",
+              "clock"
+              "notifications"
+            ]
+          }
+        },
+        "bar.launcher.icon": "",
+      }
+    '';
+  };
 }
