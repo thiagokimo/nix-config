83d071f70c0f66cc82d809007915251d82688999
Thiago Rocha <kimo@kimo.io>
2024-10-01 13:52:31 -0300

    Updating mime types

--- home/system/mime/default.nix
+++ home/system/mime/default.nix
@@ -6,6 +6,13 @@
     defaultApplications = {
       "text/markdown" = "nvim.desktop";
       "text/plain" = "nvim.desktop";
+      "text/html" = "google-chrome-stable.desktop";
+      "image/png" = "google-chrome-stable.desktop";
+      "image/jpeg" = "google-chrome-stable.desktop";
+      "image/gif" = "google-chrome-stable.desktop";
+      "x-scheme-handler/http" = "google-chrome-stable.desktop";
+      "x-scheme-handler/https" = "google-chrome-stable.desktop";
+      "application/pdf" = "google-chrome-stable.desktop";
     };
   };
 }
