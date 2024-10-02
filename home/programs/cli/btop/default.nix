2662df8cbc135136146863465672c81043e7729c
Thiago Rocha <kimo@kimo.io>
2024-10-02 14:25:54 -0300

    Fixes typos

--- home/programs/cli/btop/default.nix
+++ home/programs/cli/btop/default.nix
@@ -4,10 +4,10 @@
   };
 
   home.file = {
-    ".config/btop/btop.conf" {
+    ".config/btop/btop.conf" = {
       text = builtins.readFile ./btop.conf;
     };
-    ".config/btop/themes/gruvbox_dark.theme" {
+    ".config/btop/themes/gruvbox_dark.theme" = {
       text = builtins.readFile ./gruvbox_dark.theme;
     };
   };
