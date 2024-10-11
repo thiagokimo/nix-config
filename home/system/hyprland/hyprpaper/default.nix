911cc61cb14ab181286c855a7be415d3cc83a449
Thiago Rocha <kimo@kimo.io>
2024-10-11 14:59:37 -0300

    Enable hyprpaper service

--- home/system/hyprland/hyprpaper/default.nix
+++ home/system/hyprland/hyprpaper/default.nix
@@ -1,4 +1,12 @@
 {
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      ipc = "on";
+      splash = false;
+      splash_offset = 2.0;
+    };
+  };
   home.file.".config/hypr/hyprpaper.conf" = {
     text = builtins.readFile ./hyprpaper.conf;
   };
