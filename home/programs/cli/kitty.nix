eaf459badac4398c672952e27284f26f8e510aec
Thiago Rocha <kimo@kimo.io>
2024-10-15 15:28:25 -0300

    Adding a few customizations to kitty

--- home/programs/cli/kitty.nix
+++ home/programs/cli/kitty.nix
@@ -2,5 +2,19 @@
   programs.kitty = {
     enable = true;
     themeFile = "GruvboxMaterialDarkHard";
+    settings = {
+      scrollback_lines = 10000;
+      initial_window_width = 1200;
+      initial_window_height = 600;
+      update_check_interval = 0;
+      enable_audio_bell = false;
+      confirm_os_window_close = "0";
+      remember_window_size = "no";
+      disable_ligatures = "never";
+      url_style = "curly";
+      cursor_shape = "Underline";
+      cursor_underline_thickness = 3;
+      window_padding_width = 10;
+    };
   };
 }
