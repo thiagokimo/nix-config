f4c647d184639ed623606cbc18c336a771be9869
Thiago Rocha <kimo@kimo.io>
2024-10-28 10:13:41 -0300

    Update gruvbox colors

--- themes/gruvbox-dark.nix
+++ themes/gruvbox-dark.nix
@@ -7,22 +7,22 @@
     enable = true;
 
     base16Scheme = {
-      base00 = "202020";
-      base01 = "2a2827";
-      base02 = "504945";
-      base03 = "5a524c";
-      base04 = "bdae93";
-      base05 = "ddc7a1";
-      base06 = "ebdbb2";
-      base07 = "fbf1c7";
-      base08 = "ea6962";
-      base09 = "e78a4e";
-      base0A = "d8a657";
-      base0B = "a9b665";
-      base0C = "89b482";
-      base0D = "7daea3";
-      base0E = "d3869b";
-      base0F = "bd6f3e";
+      base00: "1d2021" # ----
+      base01: "3c3836" # ---
+      base02: "504945" # --
+      base03: "665c54" # -
+      base04: "bdae93" # +
+      base05: "d5c4a1" # ++
+      base06: "ebdbb2" # +++
+      base07: "fbf1c7" # ++++
+      base08: "fb4934" # red
+      base09: "fe8019" # orange
+      base0A: "fabd2f" # yellow
+      base0B: "b8bb26" # green
+      base0C: "8ec07c" # aqua/cyan
+      base0D: "83a598" # blue
+      base0E: "d3869b" # purple
+      base0F: "d65d0e" # brown
     };
 
     cursor = {
