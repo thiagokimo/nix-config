723b6c3223b4586f49e8d8eb33b7aba0172eb37d
Thiago Rocha <kimo@kimo.io>
2024-10-28 10:55:27 -0300

    Move theming variables into its own file

--- hosts/framework/variables.nix
+++ hosts/framework/variables.nix
@@ -16,16 +16,6 @@
     defaultLocale = "en_US.UTF-8";
     extraLocale = "pt_BR.UTF-8";
 
-    # TODO Add more theme stuff
-    theme = {
-      rounding = 15;
-      gaps-in = 10;
-      gaps-out = 10 * 2;
-      active-opacity = 1;
-      inactive-opacity = 0.89;
-      blur = true;
-      border-size = 3;
-      layout = "dwindle";
-    };
+    theme = import ../../themes/var/gruvbox-dark.nix;
   };
 }
