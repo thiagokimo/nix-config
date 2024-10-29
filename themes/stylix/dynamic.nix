e94346da0a9e31cc30eb23be58efb56ee51215ee
Thiago Rocha <kimo@kimo.io>
2024-10-29 16:36:21 -0300

    Uses stylix with wallpaper colors

---
+++ themes/stylix/dynamic.nix
@@ -0,0 +1,43 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  stylix = {
+    enable = true;
+    
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 24;
+    };
+
+    fonts = {
+      monospace = {
+        package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
+        name = "JetBrainsMono Nerd Font";
+      };
+      sansSerif = {
+        package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
+        name = "JetBrainsMono Nerd Font";
+      };
+      serif = {
+        package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
+        name = "JetBrainsMono Nerd Font";
+      };
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+      sizes = {
+        applications = 13;
+        desktop = 13;
+        popups = 13;
+        terminal = 13;
+      };
+    };
+
+    polarity = "dark";
+    image = inputs.nix-wallpapers + "/gruvbox/15.png";
+  };
+}
