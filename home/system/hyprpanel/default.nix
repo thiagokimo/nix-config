4625c4b7e71126fe36e120b200edc05fa5dad2c9
Thiago Rocha <kimo@kimo.io>
2024-10-29 16:08:28 -0300

    Fixes thunar shortcuts

--- home/system/hyprpanel/default.nix
+++ home/system/hyprpanel/default.nix
@@ -11,6 +11,7 @@
   gaps-in = config.var.theme.gaps-in;
   location = config.var.location;
   wallpaper = config.stylix.image;
+  polarity = config.stylix.polarity;
 in {
   wayland.windowManager.hyprland.settings.exec-once = [
     "${pkgs.hyprpanel}/bin/hyprpanel"
@@ -18,7 +19,6 @@ in {
   home.packages = with pkgs; [hyprpanel libnotify];
   home.file.".cache/ags/hyprpanel/options.json".text = ''
     {
-      "bar.custommodules.updates.pollinginterval":1440000,
       "bar.launcher.icon":"",
       "bar.workspaces.show_numbered": false,
       "scalingpriority":"hyprland",
@@ -27,13 +27,13 @@ in {
       "menus.clock.weather.location":"${location}",
       "menus.dashboard.shortcuts.left.shortcut1.icon":"",
       "menus.dashboard.shortcuts.left.shortcut1.command":"google-chrome-stable",
-      "menus.dashboard.shortcuts.left.shortcut1.tooltip":"google chrome",
-      "menus.dashboard.directories.left.directory1.command":"bash -c \"thunar $home/downloads/\"",
-      "menus.dashboard.directories.left.directory2.command":"bash -c \"thunar $home/videos/\"",
-      "menus.dashboard.directories.left.directory3.command":"bash -c \"thunar $home/projects/\"",
-      "menus.dashboard.directories.right.directory1.command":"bash -c \"thunar $home/documents/\"",
-      "menus.dashboard.directories.right.directory2.command":"bash -c \"thunar $home/pictures/\"",
-      "menus.dashboard.directories.right.directory3.command":"bash -c \"thunar $home/\"",
+      "menus.dashboard.shortcuts.left.shortcut1.tooltip":"Google Chrome",
+      "menus.dashboard.directories.left.directory1.command":"bash -c \"thunar $Home/Downloads/\"",
+      "menus.dashboard.directories.left.directory2.command":"bash -c \"thunar $Home/Videos/\"",
+      "menus.dashboard.directories.left.directory3.command":"bash -c \"thunar $Home/Projects/\"",
+      "menus.dashboard.directories.right.directory1.command":"bash -c \"thunar $Home/Documents/\"",
+      "menus.dashboard.directories.right.directory2.command":"bash -c \"thunar $Home/Pictures/\"",
+      "menus.dashboard.directories.right.directory3.command":"bash -c \"thunar $Home/\"",
       "menus.dashboard.shortcuts.right.shortcut3.command":"hyprpicker -a",
       "menus.dashboard.shortcuts.left.shortcut4.command":"menu",
       "bar.layouts":{
@@ -59,7 +59,7 @@ in {
       "theme.font.name": "${font}",
       "theme.font.size": "${fontsize}px",
       "theme.matugen": true,
-      "theme.matugen_settings.mode": "dark",
+      "theme.matugen_settings.mode": ${polarity},
       "theme.matugen_settings.scheme_type": "tonal-spot",
       "theme.matugen_settings.variation": "standard_1"
     } 
