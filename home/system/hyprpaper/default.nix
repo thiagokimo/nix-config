c023262b80f1948af8e98f48a04dc56ce8931cdc
Thiago Rocha <kimo@kimo.io>
2024-10-30 10:50:40 -0300

    Let stylix set the wallpaper

--- home/system/hyprpaper/default.nix
+++ home/system/hyprpaper/default.nix
@@ -1,14 +1,10 @@
-{ config, ...}: let
-  wallpaper = config.stylix.image;
-in {
+{
   services.hyprpaper = {
     enable = true;
     settings = {
       ipc = "on";
       splash = false;
       splash_offset = 2.0;
-      wallpaper = "${wallpaper}";
-      preload = "${wallpaper}";
     };
   };
 }
