8807f25211d4e38f92b5e7fc210add91b54462cf
Thiago Rocha <kimo@kimo.io>
2025-01-11 20:16:38 -0300

    Removing colors from wofi

--- modules/home-manager/system/wofi.nix
+++ modules/home-manager/system/wofi.nix
@@ -5,103 +5,80 @@
   ...
 }: let
   font = config.stylix.fonts.serif.name;
-  rounding = config.var.theme.rounding;
   font-size = config.stylix.fonts.sizes.popups;
-
-  background = "#${config.lib.stylix.colors.base00}";
-  background-alt = "#${config.lib.stylix.colors.base01}";
-  foreground = "#${config.lib.stylix.colors.base05}";
-  accent = "#${config.lib.stylix.colors.base0B}";
+  rounding = config.var.theme.rounding;
 in {
   home.packages = [pkgs.wofi-emoji];
 
   programs.wofi = {
     enable = true;
     settings = {
-      allow_markup = true;
-      show = "drun";
-      prompt = "Apps";
-      normal_window = true;
-      layer = "top";
-      term = "foot";
-      orientation = "vertical";
-      halign = "fill";
-      line_wrap = "off";
-      dynamic_lines = false;
       allow_images = true;
-      image_size = 24;
+      allow_markup = true;
+      dynamic_lines = false;
       exec_search = false;
-      hide_search = false;
-      parse_search = false;
-      insensitive = true;
+      halign = "fill";
       hide_scroll = true;
-      no_actions = true;
+      hide_search = false;
+      image_size = 24;
+      insensitive = true;
       key_exit = "Escape";
       key_expand = "Tab";
+      layer = "top";
+      line_wrap = "off";
+      no_actions = true;
+      normal_window = true;
+      orientation = "vertical";
+      parse_search = false;
+      prompt = "Apps";
+      show = "drun";
       sort_order = "default";
+      term = "foot";
     };
 
-    style = with config.lib.stylix.colors;
-      lib.mkForce ''
+    style = lib.mkForce ''
+      * {
+        font-family: "${font}";
+        font-weight: 500;
+        font-size: ${toString font-size}px;
+      }
 
-        * {
-          font-family: "${font}";
-          font-weight: 500;
-          font-size: ${toString font-size}px;
-        }
+      #window {
+        border-radius: ${toString rounding}px;
+      }
 
-        #window {
-          background-color: ${background};
-          color: ${foreground};
-          border-radius: ${toString rounding}px;
-        }
+      #unselected {}
 
-        #unselected {}
+      #selected {}
 
-        #selected {}
+      #img {
+        padding-right: 8px;
+      }
 
-        #img {
-          padding-right: 8px;
-        }
+      #inner-box {}
 
-        #inner-box {}
+      #outer-box {
+        padding: 20px;
+      }
 
-        #outer-box {
-          padding: 20px;
-        }
+      #entry {
+        padding: 6px;
+      }
+        
+      #entry:selected {
+        border-radius: ${toString rounding}px;
+      }
 
-        #entry {
-          padding: 6px;
-        }
+      #input {
+        border-radius: ${toString rounding}px;
+        padding: 8px 12px;
+      }
 
-        #entry:nth-child(odd) {
-          background-color: ${base00};
-        }
+      #input:focus {}
 
-        #entry:nth-child(even) {
-          background-color: ${base01};
-        }
-
-        #entry:selected {
-          background-color: ${base02};
-          border-radius: ${toString rounding}px;
-        }
-
-        #input {
-          background-color: ${background-alt};
-          color: ${base04};
-          border-radius: ${toString rounding}px;
-          border-color: ${base02};
-          padding: 8px 12px;
-        }
-
-        #input:focus {
-          border-color: ${accent};
-        }
-
-        #scroll {
-          margin-top: 20px;
-        }
-      '';
+      #scroll {
+        margin-top: 20px;
+      }
+    '';
   };
 }
