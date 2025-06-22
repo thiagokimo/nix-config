f16572725dedfbc0373a5ffdc10ff2ae6d291d27
Thiago Rocha <kimo@kimo.io>
2025-06-22 20:17:03 -0300

    Comment out fzf color usage from stylix

--- modules/home-manager/programs/cli/fzf.nix
+++ modules/home-manager/programs/cli/fzf.nix
@@ -3,22 +3,22 @@
   lib,
   ...
 }: let
-  accent = "#" + config.lib.stylix.colors.base0D;
-  foreground = "#" + config.lib.stylix.colors.base05;
-  muted = "#" + config.lib.stylix.colors.base03;
+  # accent = "#" + config.lib.stylix.colors.base0D;
+  # foreground = "#" + config.lib.stylix.colors.base05;
+  # muted = "#" + config.lib.stylix.colors.base03;
 in {
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
 
-    colors = lib.mkForce {
-      "fg+" = accent;
-      "bg+" = "-1";
-      "fg" = foreground;
-      "bg" = "-1";
-      "prompt" = muted;
-      "pointer" = accent;
-    };
+    # colors = lib.mkForce {
+    #   "fg+" = accent;
+    #   "bg+" = "-1";
+    #   "fg" = foreground;
+    #   "bg" = "-1";
+    #   "prompt" = muted;
+    #   "pointer" = accent;
+    # };
 
     defaultOptions = [
       "--margin=1"
