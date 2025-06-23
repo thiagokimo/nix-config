dd2f44f833ef14704e9311b4b4f1090a05fda024
Thiago Rocha <kimo@kimo.io>
2025-06-23 15:45:33 -0300

    Fixes kitty desktop entry to wrap executable with nixGLMesa

--- hosts/penguin/kitty-desktop-entry.nix
+++ hosts/penguin/kitty-desktop-entry.nix
@@ -1,13 +1,18 @@
 {pkgs, ...} : {
-  pkgs.mkDesktopItem {
-    name = "kitty";
-    exec = "nixGLMesa ${pkgs.kitty}/bin/kitty";
-    desktopName = "Kitty";
-    genericName = "Terminal Emulator";
-    comment = "Kitty terminal emulator";
-    categories = ["System" "TerminalEmulator"];
-    keywords = ["terminal" "console" "shell" "command" "prompt"];
-    startupNotify = true;
-    startupWMClass = "kitty";
-  };
+  home.file.".local/share/applications/kitty.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Version=1.0
+    Name=Kitty
+    GenericName=Terminal Emulator
+    Comment=Fast, feature-rich, cross-platform terminal emulator
+    Icon=kitty
+    TryExec=${pkgs.nixgl.nixGLMesa}/bin/nixGLMesa ${pkgs.kitty}/bin/kitty
+    Exec=${pkgs.nixgl.nixGLMesa}/bin/nixGLMesa ${pkgs.kitty}/bin/kitty
+    Terminal=false
+    Categories=System;TerminalEmulator;
+    Keywords=terminal;console;shell;command;prompt;
+    StartupNotify=true
+    StartupWMClass=kitty
+  '';
 }
