7ce4fd644ebda81760950f1c5c6249b97d053bb6
Thiago Rocha <kimo@kimo.io>
2025-09-08 15:32:27 -0300

    [T14] - Adding initial hm config for t14

--- home/thiago/home.nix
+++ home/thiago/home.nix
@@ -1,1 +1,18 @@
-{}: {}
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  home = {
+    username = lib.mkDefault "thiago";
+    homeDirectory = lib.mkDefault "/home/${config.home.username}";
+    stateVersion = "25.05";
+    packages = with pkgs; [
+      cowsay
+    ];
+    sessionVariables = {};
+    file = {};
+  };
+  programs.home-manager.enable = true;
+}
