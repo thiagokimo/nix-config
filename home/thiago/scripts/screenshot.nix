97bc609f7fcb8428deead5d17043b878b4deaa44
Thiago Rocha <kimo@kimo.io>
2025-09-10 10:59:26 -0300

    Fixes pictures dir path in screenshot script

--- home/thiago/scripts/screenshot.nix
+++ home/thiago/scripts/screenshot.nix
@@ -4,10 +4,10 @@
   ...
 }: let
   screenshot = pkgs.writeShellScriptBin "screenshot" ''
-    ${pkgs.hyprshot}/bin/hyprshot -m output -o ${config.var.picturesDirectory}
+    ${pkgs.hyprshot}/bin/hyprshot -m output -o ${config.home.homeDirectory}/Pictures
   '';
   screenshot-region = pkgs.writeShellScriptBin "screenshot-region" ''
-    ${pkgs.hyprshot}/bin/hyprshot -m region -o ${config.var.picturesDirectory}
+    ${pkgs.hyprshot}/bin/hyprshot -m region -o ${config.home.homeDirectory}/Pictures
   '';
 in {
   home.packages = [
