2da5b2aa3f37c2f11f48a0eca3fa1b1c7ba38791
Thiago Rocha <kimo@kimo.io>
2025-09-15 14:21:42 -0300

    Add lock script

--- home/thiago/scripts/default.nix
+++ home/thiago/scripts/default.nix
@@ -3,6 +3,7 @@
     ./app-launcher.nix
     ./nix-kimo.nix
     ./brightness.nix
+    ./lock.nix
     ./screenshot.nix
     ./sound.nix
   ];
---
+++ home/thiago/scripts/lock.nix
@@ -0,0 +1,10 @@
+{pkgs, ...}: let
+  lock-hypr = pkgs.writeShellScriptBin "lock-hypr" ''
+    ${pkgs.hyprlock}/bin/hyprlock
+  '';
+in {
+  home.packages = [
+    pkgs.hyprlock
+    lock-hypr
+  ];
+}
