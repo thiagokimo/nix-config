fa156ec6c610bde5267893498fba220bce5f7d30
Thiago Rocha <kimo@kimo.io>
2025-09-25 11:44:39 -0300

    Adds a few configs to dunst

--- home/thiago/programs/dunst.nix
+++ home/thiago/programs/dunst.nix
@@ -1,3 +1,24 @@
 {
-  services.dunst.enable = true;
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        format = "<b>%s %p</b>\n%b";
+        sort = "yes";
+        indicate_hidden = true;
+        idle_threshold = 0;
+        width = 300;
+        alignment = "center";
+        show_age_threshold = 40;
+        sticky_history = "yes";
+        follow = "mouse";
+        word_wrap = "yes";
+        separator_height = 2;
+        horizontal_padding = 10;
+        monitor = 0;
+        frame_width = 3;
+        offset = "(20,20)";
+      };
+    };
+  };
 }
