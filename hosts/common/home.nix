ac0526f67fddf5afbb67a4b574eaec99934f0cdf
Thiago Rocha <kimo@kimo.io>
2025-10-22 13:40:53 -0300

    Duplicate nixpkgs configs in common hosts home

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -1,7 +1,20 @@
-{configVars, ...}: {
+{outputs, configVars, ...}: {
   imports = [
     ../../modules/home/cli
   ];
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+    overlays = with outputs.overlays; [
+      additions
+      modifications
+      stable-packages
+    ];
+  };
+
 
   home = {
     username = "${configVars.username}";
