810faeda98a1d79dff30d09855565e3716ff9d31
Thiago Rocha <kimo@kimo.io>
2025-10-25 19:42:39 -0300

    [waybar] Update configs and styles

--- modules/home/programs/waybar/style.nix
+++ modules/home/programs/waybar/style.nix
@@ -2,6 +2,7 @@
   font = "${config.stylix.fonts.serif.name}";
   background = "#${config.lib.stylix.colors.base01}";
   hard-background = "#${config.lib.stylix.colors.base00}";
+  soft-background = "#${config.lib.stylix.colors.base07}";
   foreground = "#${config.lib.stylix.colors.base06}";
   red = "#${config.lib.stylix.colors.base08}";
   orange = "#${config.lib.stylix.colors.base09}";
@@ -11,125 +12,68 @@
   blue = "#${config.lib.stylix.colors.base0D}";
   purple = "#${config.lib.stylix.colors.base0E}";
   brown = "#${config.lib.stylix.colors.base0F}";
+
+  side-margin = "4px";
 in {
   programs.waybar.style = ''
     * {
       font-family: ${font};
     }
-    #waybar {
-      box-shadow: 10px 10px 10px 10px #000000;
+    
+    #tray {
+      padding-left: 4px;
     }
-    window#waybar {
-      box-shadow: 10px 10px 10px 10px #000000;
-      background-color: ${background};
-      color: ${foreground};
-      border-top: 3px solid;
-      border-bottom: 3px solid;
-      border-color: ${hard-background};
+    
+    #network {
+      color: ${green};
+      margin-left: ${side-margin};
+      margin-right: ${side-margin};
     }
-    tooltip {
-      background: ${background};
-      font-size: 10px;
-      opacity: 0.9;
-      border-radius: 0px;
-      border-width: 2px;
-      border-style: solid;
-      border-color: ${hard-background};
-    }
-    .modules-right {
-      font-size: 9.8px;
-      border-top: 3px solid;
-      border-right: 3px solid;
-      border-bottom: 3px solid;
-      border-color: ${hard-background};
-    }
-
-    .modules-center {
-      border-top: 3px solid;
-      border-bottom: 3px solid;
-      border-color: ${hard-background};
-    }
-
-    .modules-left {
-      background-color: ${background};
-      border-top: 3px solid;
-      border-left: 3px solid;
-      border-bottom: 3px solid;
-      border-color: ${hard-background};
-    }
-
-    #workspaces {
-      background-color: ${background};
-      color: ${foreground};
-      padding-left: 1em;
-      font-size: 9.9px;
-    }
-    #workspaces button {
-      color: ${foreground};
-      padding: 0 6px;
-      font-size: 9.9px;
-    }
-    #workspaces button:hover {
-      background-color: ${aqua};
-      color: #000000;
-    }
-    #workspaces button.visible.active {
-      color: ${red};
-      font-size: 12.5px;
-    }
-    #workspaces button.visible {
-      color: ${aqua};
-    }
-    #workspaces button.active:hover {
-      background-color: ${aqua};
-      color: #000000;
-      font-size: 12px;
-    }
-
-    #clock {
-      font-size: 10px;
-      background-color: ${background};
-      padding-right: 1em;
-      padding-top: 3px;
-      padding-left: 1em;
-      border-right: 3px;
-      border-right-style: solid;
-      border-right-color: ${yellow};
+        
+    #wireplumber {
+      color: ${purple};
+      margin-left: ${side-margin};
+      margin-right: ${side-margin};
     }
 
     #battery {
-      color: ${foreground};
-      padding-left: 0.5em;
-      padding-right: 1.5em;
-    }
-    #battery.charging,
-    #battery.plugged {
-      color: ${foreground};
+      color: ${yellow};
+      margin-left: ${side-margin};
+      margin-right: ${side-margin};
     }
 
-    #memory {
-      padding-left: 1em;
-      padding-right: 1em;
+    #bluetooth {
+      color: ${blue};
+      margin-left: ${side-margin};
+      margin-right: ${side-margin};
     }
-
+    
     #cpu {
-      padding-right: 1em;
-      padding-left: 1.5em;
-      border-left: 3px;
-      border-left-style: solid;
-      border-left-color: ${green};
+      color: ${brown};
+      margin-left: ${side-margin};
+      margin-right: ${side-margin};
     }
-
-    #network {
-      padding-right: 2em;
-      padding-left: 1em;
+    
+    #temperature {
+      color: ${yellow};
+      margin-left: ${side-margin};
+      margin-right: ${side-margin};
     }
-
-    #wireplumber.muted,
-    #wireplumber {
-      background-color: ${background};
-      padding-right: 1em;
-      padding-left: 1.5em;
+    
+    #memory {
+      color: ${blue};
+      margin-left: ${side-margin};
+      margin-right: ${side-margin};
+    }
+    
+    #custom-powermenu {
+      color: ${red};
+      margin-left: ${side-margin};
+      margin-right: ${side-margin};
+    }
+    #custom-separator {
+      color: ${soft-background};
+      margin: 0 5px;
     }
   '';
 }
