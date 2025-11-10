21750771a60c9939fa3b3723707eb9f060bc192e
Thiago Rocha <kimo@kimo.io>
2025-11-10 10:18:51 -0300

    [Waybar] - Update style and position of modules

--- modules/home/programs/waybar/config.nix
+++ modules/home/programs/waybar/config.nix
@@ -7,25 +7,39 @@
       position = "top";
 
       modules-left = [
-        "clock"
-        "custom/separator"
-        "hyprland/workspaces"
-        "custom/separator"
-        "tray"
+        "custom/launcher"
+        "backlight"
+        "wireplumber"
+        "cpu"
+        "mpris"
       ];
-      modules-center = ["hyprland/window"];
+      modules-center = [
+        "hyprland/workspaces"
+        "clock"
+      ];
 
       modules-right = [
-        "temperature"
-        "memory"
-        "cpu"
-        "wireplumber"
+        "wlr/taskbar"
         "bluetooth"
-        "battery"
         "network"
-        "custom/separator"
-        "custom/powermenu"
+        "memory"
+        "tray"
+        "battery"
       ];
+
+      "custom/launcher" = {
+        format = " ";
+        on-click = "app-launcher";
+      };
+      "backlight" = {
+        device = "intel_backlight";
+        rotate = 0;
+        format = "{icon} {percent}%";
+        format-icons = "";
+        on-scroll-up = "brightnessctl set 1%+";
+        on-scroll-down = "brightnessctl set 1$-";
+        min-length = 6;
+      };
       "hyprland/workspaces" = {
         separate-outputs = false;
         active-only = false;
@@ -33,6 +47,7 @@
         all-outputs = true;
         format = "{icon}";
         on-click = "activate";
+        sort-by-number = true;
 
         format-icons = {
           "1" = "1";
@@ -44,17 +59,19 @@
           "7" = "7";
           "8" = "8";
           "9" = "9";
+          "10" = "10";
         };
       };
-      "temperature" = {
-        format = "{icon} {temperatureC}°C ";
-        tooltip = false;
-        critical-threshold = 80;
-        format-icons = ["" "" ""];
-      };
+      # "temperature" = {
+      #   format = "{icon} {temperatureC}°C ";
+      #   tooltip = false;
+      #   critical-threshold = 80;
+      #   format-icons = ["" "" ""];
+      # };
       "tray" = {
         icon-size = 16;
-        spacing = 8;
+        rotate = 0;
+        spacing = 5;
       };
       "hyprland/window" = {
         format = "{initialTitle}";
@@ -62,6 +79,7 @@
         icon-size = 16;
       };
       "wireplumber" = {
+        tooltip = false;
         format = "{icon} {volume}%";
         format-muted = "  {volume}%";
         format-icons = {
@@ -82,33 +100,41 @@
       };
       "network" = {
         format-wifi = " ";
-        format-disconnected = "⚠";
+        rotate = 0;
+        format-ethernet = "󰈀 ";
+        format-disconnected = "󰖪 ";
         on-click = "kitty --class nmtui -e nmtui";
-        tooltip-format = "{essid}";
+        tooltip-format = "Network: <big><b>{essid}</b></big>\nSignal strength: <b>{signaldBm}dBm ({signalStrength}%)</b>\nFrequency: <b>{frequency}MHz</b>\nInterface: <b>{ifname}</b>\nIP: <b>{ipaddr}/{cidr}</b>\nGateway: <b>{gwaddr}</b>\nNetmask: <b>{netmask}</b>";
+        format-linked = "󰈀 {ifname} (No IP)";
+        tooltip-format-disconnected = "Disconnected";
+        interval = 2;
       };
       "cpu" = {
         format = "  {usage}%";
         interval = 1;
         tooltip = false;
+        on-click = "kitty --class btop -e btop";
       };
       "memory" = {
-        interval = 1;
-        format = "  {}%";
+        interval = 30;
+        format = "  {used:0.1f}G/{total:0.1f}G%";
+        max-length = 20;
         tooltip = false;
       };
 
       "battery" = {
         interval = 1;
         states = {
-          good = 90;
-          warning = 15;
-          critical = 5;
+          good = 95;
+          warning = 30;
+          critical = 20;
         };
         format = "{icon} {capacity}%";
-        format-full = "{icon} {capacity}%";
+        rotate = 0;
         format-charging = " {capacity}%";
         format-plugged = " {capacity}%";
-        format-icons = [" " " " " " " " " "];
+        format-alt = "{time} {icon}";
+        format-icons = ["󰂎" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹"];
       };
       "clock" = {
         format = " 󰸗 {:%I:%M %p} ";
@@ -116,15 +142,17 @@
         tooltip = true;
         tooltip-format = "{:%d %B %H:%M}";
       };
-      "custom/powermenu" = {
-        format = "  ";
-        tooltip = false;
-        # on-click = "exec wlogout -p layer-shell";
-      };
-      "custom/separator" = {
-        format = "|";
-        interval = "once";
-        tooltip = false;
+      "wlr/taskbar" = {
+        format = "{icon}";
+        icon-size = 16;
+        icon-theme = "Gruvbox-Dark-Hard";
+        tooltip-format = "{title}";
+        on-click = "activate";
+        on-click-middle = "close";
+        ignore-list = ["kitty"];
+        # app_ids-mapping = {};
+        markup = true;
+        all-outputs = false;
       };
     };
   };
