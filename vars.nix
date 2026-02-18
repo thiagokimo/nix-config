5bd47bc6bbf518bab097a343839714f90d5d3727
Thiago Rocha <kimo@kimo.io>
2026-02-18 12:35:37 -0300

    Isolate helpers and variables

---
+++ vars.nix
@@ -0,0 +1,8 @@
+{
+  username = "thiago";
+  stateVersion = "25.11";
+  email = "kimo@kimo.io";
+  path = "/home/${username}/.config/nix-config";
+  editor = "nvim";
+  browser = "google-chrome-stable";
+}
