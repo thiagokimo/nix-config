ef094cfe365a038cb17c4b6715563842a606b87e
Thiago Rocha <kimo@kimo.io>
2026-03-09 11:51:04 -0300

    Fixes user group permissions for kanata

--- modules/nixos/services/kanata.nix
+++ modules/nixos/services/kanata.nix
@@ -1,5 +1,20 @@
 { config, pkgs, ...}:{
+  boot.kernelModules = [ "uinput" ];
+
   hardware.uinput.enable = true;
+
+  services.udev.extraRules = ''
+    KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+  '';
+
+  users.groups.uinput = { };
+
+  systemd.services.kanata-internalKeyboard.serviceConfig = {
+    SupplementaryGroups = [
+      "input"
+      "uinput"
+    ];
+  };
 
   services.kanata = {
     enable = true;
