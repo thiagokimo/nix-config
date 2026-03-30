f3cb4894e2531e6a425b43bee2fbc1145f676f4a
Thiago Rocha <kimo@kimo.io>
2026-03-30 10:36:17 -0300

    Import noctalia home manager modules

--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -5,6 +5,6 @@
 }: {
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
   };
 }
